d336fafdb8144cf5a4c2c01a59820ebaa8d9c6f0
Talisson Avila <talisson_avila@hotmail.com>
2022-10-03 14:50:00 -0300

    edit atoms
    editado o código de retorno de atômicas

--- lista1q5.py
+++ lista1q5.py
@@ -1,15 +1,16 @@
-# lista 1 questão 5
+# lista 1 questão 5 / Forma reduzida
 
 from funcoes import *
 
 
 def atomicas(formula):
     if isinstance(formula, Atomica):
-        return formula.nome
+        lista_de_atomicas = [formula.nome]
+        return set(lista_de_atomicas)
     if isinstance(formula, Nao):
         return atomicas(formula.dentro)
     if isinstance(formula, Implica) or isinstance(formula, E) or isinstance(formula, Ou):
-        return atomicas(formula.esquerda) + atomicas(formula.direita)
+        return atomicas(formula.esquerda) | atomicas(formula.direita)
 
 
 p = Atomica('p')
@@ -17,5 +18,11 @@ q = Atomica('q')
 s = Atomica('s')
 formula1Q5 = Ou(E(p, Nao(Implica(p, Nao(q)))), Nao(q))
 formula2Q5 = Ou(Nao(E(p, s)), p)
-print(f'A fórmula {formula1Q5} tem como atômicas {set(atomicas(formula1Q5))}')
-print(f'A fórmula {formula2Q5} tem como atômicas {set(atomicas(formula2Q5))}')
+print(f'A fórmula {formula1Q5} tem como atômicas {atomicas(formula1Q5)}')
+print(f'A fórmula {formula2Q5} tem como atômicas {atomicas(formula2Q5)}')
+
+teste1 = Atomica('x+1')
+teste2 = Atomica('x-3')
+x = Atomica('x')
+formulateste = Ou(teste1, Implica(x, teste2))
+print(f'A fórmula {formulateste} tem como atômicas {atomicas(formulateste)}')
